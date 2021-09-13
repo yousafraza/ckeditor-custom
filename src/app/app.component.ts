0359075c488e9744e76e9a0f9449eae960e779e7
Yousaf <yousafraza2107@gmail.com>
2021-09-13 19:25:53 +0500

    ckeditor custom inline toolbar, dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as ClassicEditor from 'src/ckeditor.js';
+// import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import BalloonToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/balloon/balloontoolbar';
+// import * as BaloonEditor from '@ckeditor/ckeditor5-build-balloon';
+// import tinymce from 'tinymce';
+// import tim
+
+// import {Jodit} from 'jodit';
+
+// declare var tinymce: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'med-editor';
+  Editor = ClassicEditor;
+
+  ngOnInit() {
+  
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+// import { QuillModule } from 'ngx-quill';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EditorModule,
+    CKEditorModule,
+    // QuillModule
   ],
   providers: [],
   bootstrap: [AppComponent]
