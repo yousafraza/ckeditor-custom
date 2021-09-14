877500488206c99940398b1bf04b3a0dc253e4b8
Yousaf <yousafraza2107@gmail.com>
2021-09-14 23:17:10 +0500

    ckeditor textselection plugin, custom dropdown wirt selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,65 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { ChangeEvent } from '@ckeditor/ckeditor5-angular';
 import * as ClassicEditor from 'src/ckeditor.js';
-// import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import BalloonToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/balloon/balloontoolbar';
-// import * as BaloonEditor from '@ckeditor/ckeditor5-build-balloon';
-// import tinymce from 'tinymce';
-// import tim
-
-// import {Jodit} from 'jodit';
-
-// declare var tinymce: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'med-editor';
   Editor = ClassicEditor;
+  selectedText: string;
+
+  @ViewChild( 'editor' ) editorComponent: ElementRef;
+
+  @HostListener('window: CustomDropdown', ['$event'])
+  onWriteAbout(e: any) {
+    if (this.selectedText) {
+      console.log(this.selectedText)
+    }
+  }
+
+  @HostListener('window: textSelectionDone', ['$event'])
+  onTextSelection(e: any) {
+    this.selectedText = e.selectedText.text;
+  }
+
+  ngAfterViewInit() {
+  }
 
   ngOnInit() {
-  
+    // this.Editor.builtinPlugins.map( plugin => console.log(plugin.pluginName));
+    // console.log('--------------------------------------------------------');
+    
+    // this.Editor.builtinPlugins.map( plugin => console.log(plugin.pluginName));
+    // console.log(window)
+    // this.gw.tinyMCE.init({
+    //     selector: '#mytextarea'
+    //   })
+    // const editor = Jodit.make('#editor');
+    // editor.value = '<p>start</p>';
+
+    // tinymce.init({
+    //   selector: 'textarea#full-featured-non-premium',
+    //     toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+    //     toolbar_sticky: true
+    // });
+
+    // tinymce.init({
+    //   selector: 'textarea#full-featured-non-premium',
+    //   toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+    //   toolbar_sticky: true
+    //  });
+
+    // tinymce.init({
+    //   selector: '#mytextarea'
+    // });
   }
+
+  public onChange( { editor }: ChangeEvent ) {
+    const data = editor.getData();
+    console.log( data );
+}
 }
